620ded67bdbc323b475b1fe2ed4d1145f6d93fac
Rahul Sarathy <rahul.sarathy@hipmunk.com>
2018-07-01 02:21:34 -0700

    Recognizes Macros and creates input fields

--- src/App.js
+++ src/App.js
@@ -9,17 +9,79 @@ class App extends Component {
     super();
 
     this.handleTextAreaChange = this.handleTextAreaChange.bind(this);
+    this.createMacroInputs = this.createMacroInputs.bind(this);
 
     this.state = {
-      mainTextInputValue: ""
+      mainTextInputValue: "",
+      macros: [],
+      inputValues: {}
     };
   }
 
+getInput(macro)
+{
+  var inputValues = this.state.inputValues;
+  if(inputValues.hasOwnProperty(macro))
+    return inputValues[macro];
+  else
+    return ""
+}
+
 handleTextAreaChange(event){
+  //find macros for every text event change
+
+  this.findMacros(event.target.value);
+  //this.createURLArray(event.target.value);
+  this.createMacroInputs();
   this.setState(
     {
-      mainTextInputValue: event.target.value
+      mainTextInputValue: event.target.value,
     });
+
+}
+
+
+findMacros(url){
+  var myRegexp = /{(\w+)}/g,
+  macros = [],
+  allMatches = [],
+  match;
+
+  while (match = myRegexp.exec(url))
+  {
+    allMatches.push(match[0]);
+    if (!macros.includes(match[1]))
+      macros.push(match[1]);
+  }
+  this.setState(
+  {
+    allMatches: allMatches,
+    macros: macros
+  }
+  );
+}
+
+createMacroInputs(){
+  var macros = this.state.macros;
+  console.log(macros);
+
+  const macroTitles = macros.map((macro) => 
+    <Macro onChange={(e) => this.handleMacroChange(macro, e)} value={this.state.inputValues[macro]} macro={macro} key={macro}/>
+  );
+
+  this.setState(
+    {
+      macroTitles: macroTitles
+    });
+}
+
+handleMacroChange(macro, event)
+{
+  var inputValues = this.state.inputValues;
+  inputValues[macro] = event.target.value;
+  this.setState({
+    inputValues: inputValues
+  });
 }
 
   render() {
